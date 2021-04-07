9a37366e5500be9c92ec186a2a392424204672f4
ah10240s <xrhwd975@yahoo.co.jp>
2021-04-07 15:06:54 +0900

    “add_login_function”

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,7 +1,10 @@
 class MypageController < ApplicationController
 
     def home
-
+        # ログインしてないなら、ログインページ（/users/sign_in）にリダイレクト
+        if !user_signed_in?
+            redirect_to new_user_session_path
+        end
     end
     
     def show
