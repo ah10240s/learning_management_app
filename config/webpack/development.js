5d7e0f442346a5a6af3c8a3ebae34bd006938f1d
ah10240s <xrhwd975@yahoo.co.jp>
2021-04-08 22:03:20 +0900

    “adminlte_test”

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+    $: 'admin-lte/plugins/jquery/jquery',
+    jQuery: 'admin-lte/plugins/jquery/jquery',
+    })
+)
+
+
+
 module.exports = environment.toWebpackConfig()
