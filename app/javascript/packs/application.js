ad99bf244dbb407974c4d7634978d0ad53041ecc
ah10240s <xrhwd975@yahoo.co.jp>
2021-04-09 17:49:39 +0900

    reset-adminlte

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,15 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("admin-lte");
+require('admin-lte');
+require("jquery");
+
+import '../stylesheets/application';
+import "@fortawesome/fontawesome-free/js/all";
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,17 +3,11 @@ const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
-    $: 'admin-lte/plugins/jquery/jquery',
-    jQuery: 'admin-lte/plugins/jquery/jquery',
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
   })
 )
 
 module.exports = environment
 
-environment.toWebpackConfig().merge({
-  resolve: {
-  alias: {
-    'jquery': 'admin-lte/plugins/jquery/jquery',
-    }
-  }
-});
