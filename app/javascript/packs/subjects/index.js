323c10e50d4f1a39affb1f1a051c220af33b8728
ah10240s <xrhwd975@yahoo.co.jp>
2021-04-14 14:23:16 +0900

    “create-sbject-editpage4”

--- app/javascript/packs/subjects/index.js
+++ app/javascript/packs/subjects/index.js
@@ -1,4 +1,4 @@
-import '/node_modules/admin-lte/plugins/datatables/jquery.dataTables.min.js';
+import './node_modules/admin-lte/plugins/datatables/jquery.dataTables';
 
 import '../../stylesheets/subjects/index.scss';
 
