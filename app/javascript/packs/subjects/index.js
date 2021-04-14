72aa61949bf55c7925115682ae5511750a60ff04
ah10240s <xrhwd975@yahoo.co.jp>
2021-04-14 13:27:05 +0900

    “create-sbject-editpage3”

--- app/javascript/packs/subjects/index.js
+++ app/javascript/packs/subjects/index.js
@@ -1,7 +1,7 @@
-import 'admin-lte/plugins/datatables/jquery.dataTables.min';
-import 'admin-lte/plugins/datatables-bs4/js/dataTables.bootstrap4.min';
-import 'admin-lte/plugins/datatables-responsive/js/dataTables.responsive.min';
-import 'admin-lte/plugins/datatables-responsive/js/responsive.bootstrap4.min';
+import 'admin-lte/plugins/datatables/jquery.dataTables';
+import 'admin-lte/plugins/datatables-bs4/js/dataTables.bootstrap4';
+import 'admin-lte/plugins/datatables-responsive/js/dataTables.responsive';
+import 'admin-lte/plugins/datatables-responsive/js/responsive.bootstrap4';
 
 import '../../stylesheets/subjects/index.scss';
 
