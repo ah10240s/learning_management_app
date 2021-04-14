1d85b894245e803282d5bd7f066c235fd334134d
ah10240s <xrhwd975@yahoo.co.jp>
2021-04-14 14:38:43 +0900

    “create-sbject-editpage4”

--- app/javascript/packs/subjects/index.js
+++ app/javascript/packs/subjects/index.js
@@ -1,7 +1,5 @@
 import '/node_modules/admin-lte/plugins/datatables/jquery.dataTables';
-import 'admin-lte/plugins/datatables-bs4/js/dataTables.bootstrap4';
-import 'admin-lte/plugins/datatables-responsive/js/dataTables.responsive';
-import 'admin-lte/plugins/datatables-responsive/js/responsive.bootstrap4';
+
 
 import '../../stylesheets/subjects/index.scss';
 
